01eeeffffb44e32a52c54c95d2671fb1813dc307
Werfmon <d.vyroubal.w@gmail.com>
2022-05-09 18:07:52 +0200

    registration

--- src/Actions/Auth/RegistrationUserAction.php
+++ src/Actions/Auth/RegistrationUserAction.php
@@ -4,12 +4,38 @@ declare(strict_types=1);
 namespace App\Actions\Auth;
 
 use App\Actions\Action;
+use App\Actions\ActionPayload;
 use Slim\Psr7\Response;
 
 class RegistrationUserAction extends Action
 {
     public function action(): Response
     {
-        return $this->respondWithData();
+        $body = $this->request->getParsedBody();
+
+        $password = '';
+        $passwordAgain = '';
+        if(isset($body['password']) && isset($body['passwordAgain'])) 
+        {
+            $password = $body['password'];
+            $passwordAgain = $body['passwordAgain'];
+        } else {
+            return $this->respond(new ActionPayload(500));
+        }
+
+        if(!strcmp($password, $passwordAgain))
+        {
+            try{
+                $this->connection->query('INSERT INTO user (uuid, name, surname, email, role, sex, password) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                                          (string)\Ramsey\Uuid\Uuid::uuid4(), $body['name'], $body['surname'], $body['email'], (int)$body['role'],  $body['sex'], sha1($password));
+            } catch(\Exception $e)
+            {
+                return $this->respond(new ActionPayload(500));
+            }
+        } else {
+            return $this->respond(new ActionPayload(500));
+        }
+        return $this->respond(new ActionPayload(200));
     }
+  
 }
