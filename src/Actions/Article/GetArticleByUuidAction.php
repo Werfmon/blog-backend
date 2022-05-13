017a1a780405a425de17486a95372cb964c45fd6
Werfmon <d.vyroubal.w@gmail.com>
2022-05-13 22:48:22 +0200

    article query

--- src/Actions/Article/GetArticleByUuidAction.php
+++ src/Actions/Article/GetArticleByUuidAction.php
@@ -16,22 +16,19 @@ class GetArticleByUuidAction extends Action
             'SELECT a.title AS article_name, a.description AS article_description, a.text AS article_text, 
                     a.read_time AS article_read_time, u.name AS user_name, 
                     u.surname AS user_surname, r.name AS user_role, c.name AS category_name,
-                    u.uuid AS user_uuid
+                    u.uuid AS user_uuid, (
+                        SELECT count(*)
+                        FROM liked_article 
+                        WHERE article_uuid=? 
+                        GROUP BY article_uuid
+                    ) AS likes
              FROM article a 
              INNER JOIN category c ON a.category_id = c.id 
              INNER JOIN user u ON u.uuid = a.user_uuid
              INNER JOIN role r ON r.id = u.role_id
-             WHERE a.uuid=?', $articleUuid
-        )->fetch();
-        $articleLikes = $this->connection->query(
-            'SELECT count(*) 
-             FROM liked_article 
-             WHERE article_uuid=? 
-             GROUP BY article_uuid', $articleUuid
+             WHERE a.uuid=?', $articleUuid, $articleUuid
         )->fetch();
 
-        $article['likes'] = $articleLikes;
-        
         return $this->respondWithData($article);
     }
 }
